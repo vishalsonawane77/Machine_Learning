9bec358090bde6f6d4cb839c3698cff2dd681ef7
vishalsonawane77 <157272188+vishalsonawane77@users.noreply.github.com>
2024-01-24 23:27:07 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,12 @@
+print("=====================MENU==============================")
+print("1.Nitrogen\n2.Potassium\n3.Temperature\n4.pH\n")
+
+def input():
+    a = nitrogen,potassium
+    
+    
+ch = int(input("Enter your choice:"))
+if(ch == 1):
+    print("Nitrogen")
+elif(ch == 2):
+    print("Potassium")
